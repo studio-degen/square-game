9f1e839ea983228b886c4850587428199d5b7a03
Apurv Rayate <rayateapurv@gmail.com>
2022-04-27 18:25:01 -0400

    Utility and scrambling

--- sketch.js
+++ sketch.js
@@ -11,7 +11,10 @@ let polyminoes = [[[2, 2], [3, 2], [3, 3], [4, 3], [4, 4]],
                   [[4, 5], [5, 5], [5, 6], [6, 5], [6, 6]],
                   [[5, 4], [6, 3], [6, 4]]
                   ];
-let colors = ['green', 'red', 'blue', 'pink', 'yellow', 'orange'];
+let colors = ['#4E944F', '#83BD75', '#E9EFC0', '#363062', '#827397', '#E9D5DA'];
+let val1 = 1;
+let val2 = 7;
+let rightBtn = false;
 
 function make2DArray(cols, rows){
   let arr = new Array(cols);
@@ -43,10 +46,32 @@ function setup() {
         grid[c[0]][c[1]].col.push(colors[index]);
     })
   });
+
+  scramble(10);
+}
+
+function mousePressed() {
+  if(mouseX > 2*size && mouseX < width-2*size && mouseY > 2*size && mouseY < height-2*size){
+    rotator.x = mouseX;
+    rotator.y = mouseY;
+  }
+  if(val1 != 0){
+    let rotatorMapX = map(val1, 0.5, 5.5, 2*size, width-2*size);
+    let rotatorMapY = map(val2, 5.5, 10.5, 2*size, height-2*size);
+    //console.log(rotatorMapX, rotatorMapY);
+  }else{
+    if(val2 == 10){
+      rightBtn = true;
+      val2 = 0;
+    }
+  }
+  
 }
 
 function draw() {
   background(220);
+  //console.log(map(floor(random(5)), 0, 4, 2, 6));
+  //console.log(cols, rows);
 
   for(let i = 0; i < cols; i++){
     for (let j = 0; j < rows; j++) {
@@ -60,309 +85,37 @@ function draw() {
 
 }
 
-setInterval(() => rotatomino(), 1000/15);
+setInterval(() => stepLoop(), 1000/15);
 
-function rotatomino() {
+
+function stepLoop() {
 
   for(let i = 0; i < cols; i++){
     for (let j = 0; j < rows; j++) {
       //console.log('rot');
       if(grid[i][j].isRotator){
-        let myCol = grid[i][j].col[grid[i][j].col.length - 1];
-        //console.log(myCol, grid[i][j].col);
-        if (keyIsDown(RIGHT_ARROW)) {
+        if(grid[i][j].col.length > 1) {
+          let myCol = grid[i][j].col[grid[i][j].col.length - 1];
+          //console.log(myCol, grid[i][j].col);
+
           let newpol = [];
+          if (keyIsDown(RIGHT_ARROW)) {          
+            newpol = rotatomino(i, j, 'right', myCol);
+            
+          }
 
-          //1 cell away
-          grid[i-1][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j-1].col.splice(index, 1);
-              //grid[i-1][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
+          if (keyIsDown(LEFT_ARROW)) {
+            newpol = rotatomino(i, j, 'left', myCol);
 
-          grid[i-1][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j-1].col.splice(index, 1);
-              //grid[i-1][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
-
-          grid[i][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i][j-1].col.splice(index, 1);
-              //grid[i][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+1][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+1][j-1].col.splice(index, 1);
-              //grid[i+1][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j+1);
-              newpol.push(coord);
-            }
-          });         
-
-          grid[i+1][j].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+1][j].col.splice(index, 1);
-              //grid[i+1][j].filled = false;
-              let coord = [];
-              coord.push(i);
-              coord.push(j+1);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+1][j+1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+1][j+1].col.splice(index, 1);
-              //grid[i+1][j+1].filled = false;
-              let coord = [];
-              coord.push(i-1);
-              coord.push(j+1);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i][j+1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i][j+1].col.splice(index, 1);
-              //grid[i][j+1].filled = false;
-              let coord = [];
-              coord.push(i-1);
-              coord.push(j);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-1][j+1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j+1].col.splice(index, 1);
-              //grid[i-1][j+1].filled = false;
-              let coord = [];
-              coord.push(i-1);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-1][j].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j].col.splice(index, 1);
-              //grid[i-1][j].filled = false;
-              let coord = [];
-              coord.push(i);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
-
-
-          //2 cell away
-          //top
-          grid[i-2][j-2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-2][j-2].col.splice(index, 1);
-              //grid[i-2][j-2].filled = false;
-              let coord = [];
-              coord.push(i+2);
-              coord.push(j-2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-1][j-2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j-2].col.splice(index, 1);
-              //grid[i-1][j-2].filled = false;
-              let coord = [];
-              coord.push(i+2);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i][j-2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i][j-2].col.splice(index, 1);
-              //grid[i][j-2].filled = false;
-              let coord = [];
-              coord.push(i+2);
-              coord.push(j);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+1][j-2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+1][j-2].col.splice(index, 1);
-              //grid[i+1][j-2].filled = false;
-              let coord = [];
-              coord.push(i+2);
-              coord.push(j+1);
-              newpol.push(coord);
-            }
-          });
-          
-          //right
-          grid[i+2][j-2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+2][j-2].col.splice(index, 1);
-              //grid[i+2][j-2].filled = false;
-              let coord = [];
-              coord.push(i+2);
-              coord.push(j+2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+2][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+2][j-1].col.splice(index, 1);
-              //grid[i+2][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j+2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+2][j].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+2][j].col.splice(index, 1);
-              //grid[i+2][j].filled = false;
-              let coord = [];
-              coord.push(i);
-              coord.push(j+2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+2][j+1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+2][j+1].col.splice(index, 1);
-              //grid[i+2][j+1].filled = false;
-              let coord = [];
-              coord.push(i-1);
-              coord.push(j+2);
-              newpol.push(coord);
-            }
-          });
-          
-          //bottom
-          grid[i+2][j+2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+2][j+2].col.splice(index, 1);
-              grid[i+2][j+2].filled = false;
-              let coord = [];
-              coord.push(i-2);
-              coord.push(j+2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i+1][j+2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i+1][j+2].col.splice(index, 1);
-              //grid[i+1][j+2].filled = false;
-              let coord = [];
-              coord.push(i-2);
-              coord.push(j+1);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i][j+2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i][j+2].col.splice(index, 1);
-              grid[i][j+2].filled = false;
-              let coord = [];
-              coord.push(i-2);
-              coord.push(j);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-1][j+2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-1][j+2].col.splice(index, 1);
-              //grid[i-1][j+2].filled = false;
-              let coord = [];
-              coord.push(i-2);
-              coord.push(j-1);
-              newpol.push(coord);
-            }
-          });
-
-          //left
-          grid[i-2][j+2].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-2][j+2].col.splice(index, 1);
-              //grid[i-2][j+2].filled = false;
-              let coord = [];
-              coord.push(i-2);
-              coord.push(j-2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-2][j+1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-2][j+1].col.splice(index, 1);
-              //grid[i-2][j+1].filled = false;
-              let coord = [];
-              coord.push(i-1);
-              coord.push(j-2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-2][j].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-2][j].col.splice(index, 1);
-              //grid[i-2][j].filled = false;
-              let coord = [];
-              coord.push(i);
-              coord.push(j-2);
-              newpol.push(coord);
-            }
-          });
-          
-          grid[i-2][j-1].col.forEach((c, index) => {
-            if(c == myCol){
-              grid[i-2][j-1].col.splice(index, 1);
-              //grid[i-2][j-1].filled = false;
-              let coord = [];
-              coord.push(i+1);
-              coord.push(j-2);
-              newpol.push(coord);
-            }
-          });
-          
-
+          }
 
           //console.log(newpol);
           newpol.forEach((p) => {
             grid[p[0]][p[1]].col.push(myCol);
             //grid[p[0]][p[1]].filled = true;
-          })
+          });
         }
+
        // console.log(myCol);
       }
       
@@ -370,10 +123,162 @@ function rotatomino() {
   }
 }
 
-function mousePressed() {
-  if(mouseX > 2*size && mouseX < width-2*size && mouseY > 2*size && mouseY < height-2*size){
-    rotator.x = mouseX;
-    rotator.y = mouseY;
+let rotateRight = [[[-1, -1, 1, -1], //one
+                  [0, -1, 1, 0],
+                  [1, -1, 1, 1],
+                  [1, 0, 0, 1],
+                  [1, 1, -1, 1],
+                  [0, 1, -1, 0],
+                  [-1, 1, -1, -1],
+                  [-1, 0, 0, -1]],
+
+                  [[-2, -2, 2, -2], //two //top
+                  [-1, -2, 2, -1],
+                  [0, -2, 2, 0],
+                  [1, -2, 2, 1],
+                  
+                  [2, -2, 2, 2], //right
+                  [2, -1, 1, 2],
+                  [2, 0, 0, 2],
+                  [2, 1, -1, 2], 
+                  
+                  [2, 2, -2, 2], //bottom
+                  [1, 2, -2, 1],
+                  [0, 2, -2, 0],
+                  [-1, 2, -2, -1],
+
+                  [-2, 2, -2, -2], //left
+                  [-2, 1, -1, -2],
+                  [-2, 0, 0, -2],
+                  [-2, -1, 1, -2],]];
+
+
+let rotateLeft = [[[-1, -1, -1, 1], //one
+                  [0, -1, -1, 0],
+                  [1, -1, -1, -1],
+                  [1, 0, 0, -1],
+                  [1, 1, 1, -1],
+                  [0, 1, 1, 0],
+                  [-1, 1, 1, 1],
+                  [-1, 0, 0, 1]],
+
+                  [[-2, -2, -2, 2], //two //top
+                  [-1, -2, -2, 1],
+                  [0, -2, -2, 0],
+                  [1, -2, -2, -1],
+                  
+                  [2, -2, -2, -2], //right
+                  [2, -1, -1, -2],
+                  [2, 0, 0, -2],
+                  [2, 1, 1, -2], 
+                  
+                  [2, 2, 2, -2], //bottom
+                  [1, 2, 2, -1],
+                  [0, 2, 2, 0],
+                  [-1, 2, 2, 1],
+
+                  [-2, 2, 2, 2], //left
+                  [-2, 1, 1, 2],
+                  [-2, 0, 0, 2],
+                  [-2, -1, -1, 2],]];
+
+function rotatomino(i, j, dir, myCol) {
+  let newpol = [];
+  if(dir == 'right'){
+    //1 cell away
+    rotateRight[0].forEach((colrow) => {
+      grid[i+colrow[0]][j+colrow[1]].col.forEach((c, index) => {
+        if(c == myCol){
+          grid[i+colrow[0]][j+colrow[1]].col.splice(index, 1);
+          //grid[i-1][j-1].filled = false;
+          let coord = [];
+          coord.push(i+colrow[2]);
+          coord.push(j+colrow[3]);
+          newpol.push(coord);
+        }
+      });
+    });
+
+    //2 cell away
+    rotateRight[1].forEach((colrow) => {
+      grid[i+colrow[0]][j+colrow[1]].col.forEach((c, index) => {
+        if(c == myCol){
+          grid[i+colrow[0]][j+colrow[1]].col.splice(index, 1);
+          //grid[i-1][j-1].filled = false;
+          let coord = [];
+          coord.push(i+colrow[2]);
+          coord.push(j+colrow[3]);
+          newpol.push(coord);
+        }
+      });
+    }); 
+    return newpol;
+
+  }else if(dir == 'left'){
+    //1 cell away
+    rotateLeft[0].forEach((colrow) => {
+      grid[i+colrow[0]][j+colrow[1]].col.forEach((c, index) => {
+        if(c == myCol){
+          grid[i+colrow[0]][j+colrow[1]].col.splice(index, 1);
+          //grid[i-1][j-1].filled = false;
+          let coord = [];
+          coord.push(i+colrow[2]);
+          coord.push(j+colrow[3]);
+          newpol.push(coord);
+        }
+      });
+    });
+
+    //2 cell away
+    rotateLeft[1].forEach((colrow) => {
+      grid[i+colrow[0]][j+colrow[1]].col.forEach((c, index) => {
+        if(c == myCol){
+          grid[i+colrow[0]][j+colrow[1]].col.splice(index, 1);
+          //grid[i-1][j-1].filled = false;
+          let coord = [];
+          coord.push(i+colrow[2]);
+          coord.push(j+colrow[3]);
+          newpol.push(coord);
+        }
+      });
+    }); 
+    return newpol;
+
+  }
+
+}
+
+function scramble(num) {
+  for(let i = 0; i < num; i++){
+    let x = floor(random(5));
+    let y = floor(random(5));
+    let r = random(1);
+
+    let mapX = map(x, 0, 4, 2, 6);
+    let mapY = map(y, 0, 4, 2, 6);
+
+
+    if(grid[mapX][mapY].col.length > 1) {
+      let myCol = grid[mapX][mapY].col[grid[mapX][mapY].col.length - 1];
+      //console.log(myCol, grid[i][j].col);
+
+      let newpol = [];
+
+      if(r < 0.5){
+        newpol = rotatomino(mapX, mapY, 'right', myCol);
+
+      }else{
+        newpol = rotatomino(mapX, mapY, 'left', myCol);
+
+      }
+
+      //console.log(newpol);
+      newpol.forEach((p) => {
+        grid[p[0]][p[1]].col.push(myCol);
+        //grid[p[0]][p[1]].filled = true;
+      });
+    } 
+
   }
 }
 
@@ -391,8 +296,8 @@ class Cell {
   show() {
     fill(this.col[this.col.length - 1]);
     rect(this.x, this.y, this.w, this.w);
-    fill(0);
-    text(this.col, this.x + 10, this.y + 45);
+    //fill(0);
+    //text(this.col, this.x + 10, this.y + 45);
   }
 
   rotatorCheck() {
